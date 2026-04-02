62603701215f5707c99b2f73d8e9ff9c9983aa3a
gobdanov <gobdanov@inbox.ru>
2026-04-02 09:30:34 +0500

    Разработанный пользовательский элемент для клиента. Белослудцева

---
+++ pr50_2/Elements/Item.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using pr50_2.Pages;
+
+namespace pr50_2.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        /// <summary> Данные хранилища, отображаемые в этом элементе</summary>
+        Storage Storage;
+
+        /// <summary> Ссылка на главную страницу для обновления списка после удаления</summary>
+        Main Main;
+
+        /// <summary>
+        /// Конструктор элемента
+        /// </summary>
+        /// <param name="storage">Данные для отображения</param>
+        /// <param name="main">Ссылка на главную страницу</param>
+        public Item(Storage storage, Main main)
+        {
+            InitializeComponent(); // Инициализация XAML компонентов
+
+            // Заполняем текстовые поля данными из модели
+            tbName.Text = storage.Name;      // Название сервиса
+            tbUrl.Text = storage.Url;        // URL сервиса
+            tbLogin.Text = storage.Login;    // Логин
+            tbPassword.Text = storage.Password; // Пароль
+
+            // Сохраняем ссылки для дальнейшего использования
+            this.Main = main;          // Для доступа к StorageList
+            this.Storage = storage;    // Для хранения данных
+        }
+
+        /// <summary> Обработчик нажатия кнопки "Редактировать"</summary>
+        private void Update(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Открываем страницу добавления/редактирования
+            // Передаем текущий объект Storage для редактирования
+            MainWindow.Init.OpenPages(new Pages.Add(Storage));
+        }
+
+        /// <summary> Обработчик нажатия кнопки "Удалить"</summary>
+        private void Delete(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // 1. Отправляем DELETE запрос на сервер
+            StorageContext.Delete(Storage.Id);
+
+            // 2. Удаляем элемент из визуального списка на главной странице
+            this.Main.StorageList.Children.Remove(this);
+
+            // 3. Показываем сообщение об успешном удалении
+            MessageBox.Show("Данные удалены");
+        }
+    }
+}
--- pr50_2/MainWindow.xaml.cs
+++ pr50_2/MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace pr50_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Статическая ссылка на экземпляр главного окна
+        /// Используется для доступа к методам навигации из любого места приложения
+        /// Паттерн "Одиночка" (Singleton) для простого доступа к главному окну
+        /// </summary>
+        public static MainWindow Init;
+
+        /// <summary>
+        /// Статическое поле для хранения JWT токена аутентификации
+        /// Доступно из любой точки приложения после успешного входа
+        /// </summary>
+        public static string Token;
+
+        /// <summary> Конструктор главного окна</summary>
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); // Инициализация XAML компонентов (включая frame)
+            Init = this; // Сохраняем ссылку на текущий экземпляр для статического доступа
+            // Открываем страницу логина при запуске приложения
+            OpenPages(new Pages.Login());
+        }
+
+        /// <summary>
+        /// Метод навигации между страницами
+        /// Отображает переданную страницу внутри фрейма frame
+        /// </summary>
+        /// <param name="openPage">Страница для отображения (Login, Main, Add)</param>
+        public void OpenPages(Page openPage)
+        {
+            // Используем встроенный механизм навигации WPF
+            // Frame - контейнер, который может отображать разные Page
+            frame.Navigate(openPage);
         }
     }
 }
